5848e5f0e1670ccfce0b3dcc28aee27f3824e88f
softprops <d.tangren@gmail.com>
2015-06-26 18:07:00 -0400

    docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-//#[cfg(feature= "nightly")] extern crate libc;
+#![deny(missing_docs)]
+
+//! termsize is a tiny crate that provides a simple
+//! interface for setting and retrieving the current
+//! terminal interface size
 
 extern crate libc;
 
@@ -8,6 +12,7 @@ use libc::funcs::bsd44::ioctl;
 const TIOCGWINSZ: c_ulong = 0x40087468;
 const TIOCSWINSZ: c_ulong = 2148037735;
 
+/// A representation of the size of the current terminal
 #[repr(C)]
 #[derive(Debug)]
 pub struct Size {
@@ -18,35 +23,31 @@ pub struct Size {
 }
 
 impl Size {
+  /// creates a new empty Size
   pub fn new() -> Size {
     Size { height: 0, width: 0, x: 0, y: 0 }
   }
+
+  /// sets size height
   pub fn height(&mut self, h: u16) {
     self.height = h
   }
+
+  /// sets size width
   pub fn width(&mut self, w: u16) {
     self.width = w;
   }
 }
 
+/// Sets the current terminal size
 pub fn set(s: &Size) -> bool {
-  let r = unsafe { ioctl(STDOUT_FILENO, TIOCSWINSZ, s)};
+  let r = unsafe { ioctl(STDOUT_FILENO, TIOCSWINSZ, s) };
   r == 0
 }
 
+/// Gets the current terminal size
 pub fn get() -> Option<Size> {
-  let mut s = Size::new();
+  let s = Size::new();
   let r = unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &s) };
-  match r {
-    0 => {
-      Some(s)
-    },
-    _ => {
-      None
-    }
-  }
-}
-
-#[test]
-fn it_works() {
+  if r == 0 { Some(s) } else { None }
 }
