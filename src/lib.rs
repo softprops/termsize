5dec7189bc08044587cc3e734e95db5c92311e7c
softprops <d.tangren@gmail.com>
2015-11-12 00:52:31 -0500

    add test fix example in doc

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@
 //! [terminal interface](http://www.manpagez.com/man/4/tty/) size
 //!
 //! ```rust
-//!  extern crate termsize;
-//!  termsize::get().map(|size| println!("width {} height {}", size.width, size.height));
+//! extern crate termsize;
+//! termsize::get().map(|size| {
+//!   println!("rows {} cols {}", size.rows, size.cols)
+//! });
 //! ```
 
 /// Size
@@ -27,3 +29,12 @@ pub use self::nix::get;
 mod win;
 #[cfg(windows)]
 pub use self::win::get;
+
+#[cfg(test)]
+mod tests {
+    use super::get;
+    #[test]
+    fn test_get() {
+        assert!(get().is_some())
+    }
+}
