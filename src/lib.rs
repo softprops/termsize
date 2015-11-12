dc1266fded1106996c6d7a00b177de9814f4240e
softprops <d.tangren@gmail.com>
2015-11-12 00:17:14 -0500

    restructure

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,29 @@
 #![deny(missing_docs)]
 
 //! termsize is a tiny crate that provides a simple
-//! interface for setting and retrieving the current
-//! terminal interface size
+//! interface for retrieving the current
+//! [terminal interface](http://www.manpagez.com/man/4/tty/) size
 //!
 //! ```rust
 //!  extern crate termsize;
 //!  termsize::get().map(|size| println!("width {} height {}", size.width, size.height));
 //! ```
-extern crate libc;
 
-use libc::{ c_ulong, c_ushort, STDOUT_FILENO };
-use libc::funcs::bsd44::ioctl;
-
-const TIOCGWINSZ: c_ulong = 0x40087468;
-
-/// A representation of the size of the current terminal
-#[repr(C)]
+/// Size
 #[derive(Debug)]
 pub struct Size {
-  /// number of rows
-  pub rows: c_ushort,
-  /// number of columns
-  pub cols: c_ushort,
-  x: c_ushort,
-  y: c_ushort
+    /// number of rows
+    pub rows: u16,
+    /// number of columns
+    pub cols: u16
 }
 
-impl Size {
-  /// creates a new empty Size
-  pub fn new() -> Size {
-    Size { rows: 0, cols: 0, x: 0, y: 0 }
-  }
-}
+#[cfg(unix)]
+mod nix;
+#[cfg(unix)]
+pub use self::nix::get;
 
-/// Gets the current terminal size
-pub fn get() -> Option<Size> {
-  let s = Size::new();
-  let r = unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &s) };
-  if r == 0 { Some(s) } else { None }
-}
+#[cfg(windows)]
+mod win;
+#[cfg(windows)]
+pub use self::win::get;
---
+++ src/nix.rs
@@ -0,0 +1,31 @@
+extern crate libc;
+
+use self::super::Size;
+use self::libc::{c_ulong, c_ushort, STDOUT_FILENO};
+use self::libc::ioctl;
+
+#[cfg(not(target_os = "macos"))]
+const TIOCGWINSZ: c_int = 0x00005413;
+
+#[cfg(target_os = "macos")]
+const TIOCGWINSZ: c_ulong = 0x40087468;
+
+/// A representation of the size of the current terminal
+#[repr(C)]
+#[derive(Debug)]
+pub struct UnixSize {
+  /// number of rows
+  pub rows: c_ushort,
+  /// number of columns
+  pub cols: c_ushort,
+  x: c_ushort,
+  y: c_ushort
+}
+
+
+/// Gets the current terminal size
+pub fn get() -> Option<Size> {
+  let us = UnixSize { rows: 0, cols: 0, x: 0, y: 0 };
+  let r = unsafe { ioctl(STDOUT_FILENO, TIOCGWINSZ, &us) };
+  if r == 0 { Some(Size { rows: us.rows, cols: us.cols }) } else { None }
+}
