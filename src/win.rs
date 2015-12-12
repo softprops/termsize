91194261738a0ae7eca4d27835637a7c68b1b20f
softprops <d.tangren@gmail.com>
2015-12-12 23:37:42 +0900

    clean up win code

--- src/win.rs
+++ src/win.rs
@@ -7,17 +7,15 @@ use std::ptr;
 /// Gets the current terminal size
 pub fn get() -> Option<Size> {
     //http://rosettacode.org/wiki/Terminal_control/Dimensions#Windows
-    use self::winapi::HANDLE;
-    use self::kernel32::{self, GetStdHandle, GetConsoleScreenBufferInfo, GetLastError};
+    use self::kernel32::{self, GetConsoleScreenBufferInfo};
     use self::winapi::{
         self,
-        CONSOLE_SCREEN_BUFFER_INFO, COORD, SMALL_RECT,
-        STD_OUTPUT_HANDLE, INVALID_HANDLE_VALUE
+        CONSOLE_SCREEN_BUFFER_INFO,
+        HANDLE, INVALID_HANDLE_VALUE
     };
-    let name = b"CONOUT$\0";
     let handle: HANDLE = unsafe {
         kernel32::CreateFileA(
-            name.as_ptr() as *const i8,
+            b"CONOUT$\0".as_ptr() as *const i8,
             winapi::GENERIC_READ | winapi::GENERIC_WRITE,
             winapi::FILE_SHARE_WRITE,
             ptr::null_mut(),
@@ -26,34 +24,18 @@ pub fn get() -> Option<Size> {
             ptr::null_mut(),
         )
     };
-    println!("handle {:?}", handle);
     if handle == INVALID_HANDLE_VALUE {
-        println!("handle was invalid");
         return None
     }
-    let zc = COORD { X: 0, Y: 0 };
-    // screen buffer info is a container of console display information
-    // https://msdn.microsoft.com/en-us/library/windows/desktop/ms682093(v=vs.85).aspx
-    /*CONSOLE_SCREEN_BUFFER_INFO {
-        dwSize: zc.clone(),
-        dwCursorPosition: zc.clone(),
-        wAttributes: 0,
-        srWindow: SMALL_RECT { Left:0, Top: 0, Right: 0, Bottom: 0 },
-        dwMaximumWindowSize: zc
-};*/
     let info = unsafe {
         // https://msdn.microsoft.com/en-us/library/windows/desktop/ms683171(v=vs.85).aspx
         let mut info = ::std::mem::uninitialized();
-        let result = GetConsoleScreenBufferInfo(handle, &mut info);
-        println!("result was {}", result);
-        if result == 0 {
-            println!("last error was {:?}", GetLastError());
+        if GetConsoleScreenBufferInfo(handle, &mut info) == 0 {
             None
         } else {
             Some(info)
         }
     };
-    println!("info {:?}", info);
     info.map(|inf| {
         Size {
             rows: (inf.srWindow.Bottom - inf.srWindow.Top + 1) as u16,
