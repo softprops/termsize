d8ef8eaa60b16557fa6b569b92bec91225dac7bf
softprops <d.tangren@gmail.com>
2015-12-12 23:02:31 +0900

    testing windows

--- src/win.rs
+++ src/win.rs
@@ -7,32 +7,42 @@ use self::super::Size;
 pub fn get() -> Option<Size> {
     //http://rosettacode.org/wiki/Terminal_control/Dimensions#Windows
     use self::winapi::HANDLE;
-    use self::kernel32::{GetStdHandle, GetConsoleScreenBufferInfo, GetLastError};
+    use self::kernel32::{self, GetStdHandle, GetConsoleScreenBufferInfo, GetLastError};
     use self::winapi::{
+        self,
         CONSOLE_SCREEN_BUFFER_INFO, COORD, SMALL_RECT,
         STD_OUTPUT_HANDLE, INVALID_HANDLE_VALUE
     };
-    let stdout: HANDLE = unsafe {
-        GetStdHandle(STD_OUTPUT_HANDLE)
+    let name = b"CONOUT$\0";
+    let handle: HANDLE = unsafe {
+        kernel32::CreateFileA(
+            name.as_ptr() as *const i8,
+            winapi::GENERIC_READ | winapi::GENERIC_WRITE,
+            winapi::FILE_SHARE_WRITE,
+            ptr::null_mut(),
+            winapi::OPEN_EXISTING,
+            0,
+            ptr::null_mut(),
+        )
     };
-    println!("stdout {:?}", stdout);
-    if stdout == INVALID_HANDLE_VALUE {
+    println!("handle {:?}", handle);
+    if handle == INVALID_HANDLE_VALUE {
         println!("handle was invalid");
         return None
     }
     let zc = COORD { X: 0, Y: 0 };
     // screen buffer info is a container of console display information
     // https://msdn.microsoft.com/en-us/library/windows/desktop/ms682093(v=vs.85).aspx
-    let mut info = CONSOLE_SCREEN_BUFFER_INFO {
+    let mut info = ::std::mem::uninitialized();/*CONSOLE_SCREEN_BUFFER_INFO {
         dwSize: zc.clone(),
         dwCursorPosition: zc.clone(),
         wAttributes: 0,
         srWindow: SMALL_RECT { Left:0, Top: 0, Right: 0, Bottom: 0 },
         dwMaximumWindowSize: zc
-    };
+    };*/
     let success: bool = unsafe {
         // https://msdn.microsoft.com/en-us/library/windows/desktop/ms683171(v=vs.85).aspx
-        let result = GetConsoleScreenBufferInfo(stdout, &mut info);
+        let result = GetConsoleScreenBufferInfo(handle, &mut info);
         println!("result was {}", result);
         if result == 0 {
             println!("last error was {:?}", GetLastError());
