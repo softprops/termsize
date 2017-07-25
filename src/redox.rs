f53edbfcc779bb7d24f4464479b4449e4e60a7d9
Ian Douglas Scott <ian@iandouglasscott.com>
2017-07-25 12:50:13 -0700

    Redox OS support

---
+++ src/redox.rs
@@ -0,0 +1,10 @@
+extern crate termion;
+
+use self::super::Size;
+
+/// Gets the current terminal size
+pub fn get() -> Option<Size> {
+    termion::terminal_size().ok().map(|(cols, rows)| 
+        Size { rows, cols }
+    )
+}
