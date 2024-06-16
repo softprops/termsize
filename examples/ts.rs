da685a0240cee11f1f6a4e5277dbcb943e80aead
softprops <d.tangren@gmail.com>
2024-06-16 13:01:14 -0400

    fmt

--- examples/ts.rs
+++ examples/ts.rs
@@ -1,6 +1,6 @@
 pub fn main() {
     match termsize::get() {
-       Some(size) => println!("{:?}", size),
-       _ => println!("not a term")
+        Some(size) => println!("{:?}", size),
+        _ => println!("not a term"),
     }
 }
